283acd8c370865383bdbf5f44eb0fa725a55aab2
Mike Kim <mike@Mike-Kims-Computer.local>
2018-06-26 13:08:26 -0700

    add logic to render

--- js/main.js
+++ js/main.js
@@ -2,90 +2,166 @@
 var suits = ['s', 'c', 'd', 'h'];
 var ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
 
+var nameLookup = {
+  'P': 'Player',
+  'D': 'Dealer'
+}
+
 var masterDeck = buildMasterDeck();
-var playerBankrollNum = 1000;
 
 // /*----- app's state (variables) -----*/
 var shuffledDeck, playerHand, dealerHand;
+var playerSum, dealerSum;
 var bankroll, bet;
-var handInProgress;
+var handInProgress, blackjack, winner;
 
 // /*----- cached element references -----*/
 var playerCardsEl = document.querySelector('#player-section div.cards');
-var playerScoreEl = document.querySelector('#player-section h3');
-var bankrollEl = document.querySelector('#player-section p');
+var playerScoreEl = document.querySelector('#player-section h3 span');
+var bankrollEl = document.getElementById('bankroll');
 var dealerCardsEl = document.querySelector('#dealer-section div.cards');
-var dealerScoreEl = document.querySelector('#dealer-section h3');
+var dealerScoreEl = document.querySelector('#dealer-section h3 span');
 
 var betControlEl = document.querySelector('#bet-deal-controls');
 var dealBtnEl = document.querySelector('#deal-btn');
-var hitControlEl = document.querySelector('#hit-stand-controls');
+var hitStandContolEl = document.querySelector('#hit-stand-controls');
 var playerBankroll = document.getElementsByClassName('bankroll')[0];
 
-var betFive = document.querySelector(".bet-five");
-var betTwentyFive = document.querySelector(".bet-twentyfive");
-var betHundred = document.querySelector(".bet-hundred");
-var playerBetAmount = document.querySelector(".bet-amount");
+var playerBetEl = document.querySelector(".bet-amount");
+var messageEl = document.getElementById("message");
 
 // /*----- event listeners -----*/
 dealBtnEl.addEventListener('click', handleDeal);
-betFive.addEventListener('click', betPlacedFive);
-betTwentyFive.addEventListener('click', betPlacedTwentyFive);
-betHundred.addEventListener('click', betPlacedHundred);
+document.getElementById('bet-btns').addEventListener('click', handleIncreaseBet);
+hitStandContolEl.addEventListener('click', handleHit);
+// standControlEl.addEventListener('click', handleStand);
 
 // /*----- functions -----*/
 function initialize() {
   handInProgress = false;
-  playerBankroll.innerHTML = playerBankrollNum
+  winner = null;
+  bankroll = 1000;
   bet = 0;
   render();
 }
 
 function render() {
-  bankrollEl.textContent = bankroll;
+  bankrollEl.textContent = '$' + bankroll;
+  playerBetEl.textContent = '$' + bet;
   betControlEl.style.visibility = handInProgress ? 'hidden' : 'visibile';
   bet === 0 ? dealBtnEl.setAttribute('disabled', 'disabled') : dealBtnEl.removeAttribute('disabled');
-  hitControlEl.style.visibility = handInProgress ? 'visible' : 'hidden';
+  hitStandContolEl.style.visibility = handInProgress ? 'visible' : 'hidden';
+  dealerScoreEl.textContent = handInProgress ? '?' : dealerSum;
+  playerScoreEl.textContent = playerSum;
   renderHands();
+  if (blackjack) {
+    messageEl.textContent = `${nameLookup[blackjack]} Has Blackjack!`;
+  } else if (winner) {
+    if (winner === 'T') {
+      messageEl.textContent = `It's a Push`;
+    } else {
+      messageEl.textContent = `${nameLookup[winner]} Wins!`;
+    }
+  } else {
+    messageEl.textContent = '';
+  }
 }
 
-function betPlacedFive() {
-  bet += 5;
-  playerBetAmount.innerHTML = bet;
-  playerBankrollNum -= 5;
-  playerBankroll.innerHTML = playerBankrollNum
-  render();
+// master deck
+function buildMasterDeck() {
+  var deck = [];
+  suits.forEach(function (suit) {
+    ranks.forEach(function (rank) {
+      deck.push({
+        // the 'face' property maps to the CSS classes for cards
+        face: `${suit}${rank}`,
+        // the 'value' property is set for blackjack, not war
+        value: Number(rank) || (rank === 'A' ? 11 : 10)
+      });
+    });
+  });
+  return deck;
 }
 
-function betPlacedTwentyFive() {
-  bet += 25;
-  playerBetAmount.innerHTML = bet;
-  playerBankrollNum -= 25;
-  playerBankroll.innerHTML = playerBankrollNum
-  render();
-}
-
-function betPlacedHundred() {
-  bet += 100;
-  playerBetAmount.innerHTML = bet;
-  playerBankrollNum -= 100;
-  playerBankroll.innerHTML = playerBankrollNum
-  render();
-}
-
-function handleDeal() {
+function shuffleDeck() {
   var tempDeck = masterDeck.slice();
   shuffledDeck = [];
   while (tempDeck.length) {
     var rndIdx = Math.floor(Math.random() * tempDeck.length);
     shuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
   }
-  handInProgress = true;
-  playerHand = shuffledDeck.splice(0, 2);
-  dealerHand = shuffledDeck.splice(0, 2);
+}
+
+// place bets
+function handleIncreaseBet(evt) {
+  var amt = parseInt(evt.target.textContent.replace('$', ''));
+  bet += amt;
+  bankroll -= amt;
   render();
 }
 
+// deal hands
+function handleDeal() {
+  shuffleDeck();
+  handInProgress = true;
+  winner = blackjack = null;
+  playerHand = [];
+  dealerHand = [];
+  deal(playerHand, 2);
+  deal(dealerHand, 2);
+  playerSum = computeHand(playerHand);
+  dealerSum = computeHand(dealerHand);
+  checkForBlackjack();
+  render();
+}
+
+function checkForBlackjack() {
+  if (playerSum === 21 && dealerSum === 21) {
+    messageEl = `It's a Push`;
+  } else {
+   playerSum === 21
+  }
+}
+
+
+function deal(hand, numCards) {
+  hand.push(...shuffledDeck.splice(0, numCards));
+}
+
+// comparing hands
+function computeHand(hand) {
+  var sum = 0;
+  var aces = 0;
+  for (var i = 0; i < hand.length; i++) {
+    var card = hand[i];
+    var ace = card.face.split('').includes('A')
+    if (ace) {
+      aces++;
+    }
+    sum += card.value
+  }
+  while (sum > 21 && aces) {
+    sum -= 10;
+    aces--;
+  }
+  return sum;
+}
+
+// handle hit
+function handleHit() {
+  deal(playerHand, 1);
+  playerSum = computeHand(playerHand);
+  if (playerSum > 21) {
+    winner = 'D';
+    handInProgress = false;
+    bet = 0;
+  }
+  render();
+}
+
+// stand
+
+// render hands
 function renderHands() {
   if (!playerHand) return;
   playerCardsEl.innerHTML = '';
@@ -101,21 +177,6 @@ function renderHands() {
     return html + `<div class="card ${cardClass}"></div>`;
   }, '');
   dealerCardsEl.innerHTML = cardsHtml;
-}
-
-function buildMasterDeck() {
-  var deck = [];
-  suits.forEach(function (suit) {
-    ranks.forEach(function (rank) {
-      deck.push({
-        // the 'face' property maps to the CSS classes for cards
-        face: `${suit}${rank}`,
-        // the 'value' property is set for blackjack, not war
-        value: Number(rank) || (rank === 'A' ? 11 : 10)
-      });
-    });
-  });
-  return deck;
 }
 
 initialize();
